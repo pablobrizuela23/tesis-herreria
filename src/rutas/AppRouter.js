339f8125678615b7129fde80390109a5b95c195b
pablobrizuela23 <latrell_302@hotmail.com>
2025-10-28 23:22:57 -0300

    browser

--- src/rutas/AppRouter.js
+++ src/rutas/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../paginas/Home";
 import Dashboard from "../paginas/Dashboard/Dashboard";
 import Login from "../paginas/Login";
@@ -18,7 +18,7 @@ import Notificaciones from "../paginas/Dashboard/Notificaciones";
 
 export default function AppRouter() {
   return (
-    <HashRouter basename="/tesis-herreria">
+    <BrowserRouter basename="/tesis-herreria">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -35,6 +35,6 @@ export default function AppRouter() {
         </Route>
         <Route path="/sacar-turno" element={<SacarTurnos />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
